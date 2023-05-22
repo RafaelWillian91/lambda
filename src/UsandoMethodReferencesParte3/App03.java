c3f5d9d0f4dc5f50c8a36015a129819da3daad26
rafaw <rafa.willian91@gmail.com>
2023-05-22 05:12:22 -0300

    Method Raferences part 03 (Retornando um novo Objeto)

--- src/UsandoMethodReferencesParte3/App03.java
+++ src/UsandoMethodReferencesParte3/App03.java
@@ -4,12 +4,25 @@ public class App03 {
 
     public static void main(String[] args) {
 
+        PersonCreator p1 = new PersonCreator() {
+            @Override
+            public Person creator(String name) {
+                return new Person(name);
+            }
+        };
 
+        Person p2 = p1.creator("Anonymou");
+        System.out.println(p2);
+
+
+
+        //Com Lambda
         createrPrint(s -> new Person(s), "Rafael");
+
+
 
         //Temos uma expressão lambda que tem: uma linha, um parâmetro que está sendo usado no construtor de Person
         //Temos um for candidato ao Method Reference
-
         createrPrint(Person::new, "Josiane");
 
     }
