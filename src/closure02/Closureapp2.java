6eca95cd0a91e4e2359f3988db9ab3ece87b9f83
rafaw <rafa.willian91@gmail.com>
2023-05-23 01:41:31 -0300

    Closure (variáveis externas)

--- src/closure02/Closureapp2.java
+++ src/closure02/Closureapp2.java
@@ -2,9 +2,15 @@ package closure02;
 
 public class Closureapp2 {
 
+    //Neste exemplo o closure não necessita que a variável seja final
+    private int mult;
     int calculate(int x, int y){
 
-        Operation op = (n1, n2) -> n1 + n2;
+        mult = 2;
+        Operation op = (n1, n2) -> (n1 + n2) * mult;
+        mult = 3;
+        //mult vale 3 e não 2. Pq a variável externa que faz parte da expressão lambda é definida no momento que
+        //op.calculate(x, y) é chamado. ELe só atribui mult quando o método é invocado.
         return op.calculate(x, y);
 
     }
